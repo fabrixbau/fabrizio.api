ddc81e98ab7ef51e71e95b3ba65d16b328ca2160
Fabrizio <bautistafabrizio@gmail.com>
2023-11-22 15:33:24 -0600

    proyecto creado y terminado

---
+++ src/test/java/fabrixBau/fabrizio/FabrizioApplicationTests.java
@@ -0,0 +1,13 @@
+package fabrixBau.fabrizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrizioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
